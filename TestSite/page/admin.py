7dd53d4afd48ae11ce2f90c92d21ea32ecbaf945
Oksana <oksana2000-02@mail.ru>
2023-11-16 14:08:41 +0300

    settings admin pannel

--- TestSite/page/admin.py
+++ TestSite/page/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import slider
+from django.utils.safestring import mark_safe
 # Register your models here.
+
+
+@admin.register(slider)
+class sliderAdmin(admin.ModelAdmin):
+    list_display = ['photo_display', 'description']
+
+    def photo_display(self, obj):
+        return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % obj.photo_url)
