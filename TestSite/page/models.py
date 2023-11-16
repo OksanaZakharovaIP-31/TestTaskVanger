9ec7f330d30669279fff97caa2b3657e54a2e5af
Oksana <oksana2000-02@mail.ru>
2023-11-16 13:11:47 +0300

    rename model

--- TestSite/page/models.py
+++ TestSite/page/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class photos(models.Model):
+class slider(models.Model):
     photo = models.ImageField(upload_to='img/photos')
     description = models.TextField()
 
