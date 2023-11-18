051ce01994012a66b100f0ed4dc08affc1fb3601
Oksana <oksana2000-02@mail.ru>
2023-11-18 20:46:17 +0300

    add html pages, create a header, main table in index.html

--- TestSite/page/views.py
+++ TestSite/page/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import slider
+from easy_thumbnails.files import get_thumbnailer
 from django.http import HttpResponse
 
 
@@ -6,4 +8,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('hello world!')
+    photo_slider_1 = slider.objects.all()
+    return render(request, 'page/index.html', context={'photo_slider_1': photo_slider_1})
