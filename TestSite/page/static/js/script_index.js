5150939d46f20546773d3f19658d60355f179e6b
Oksana <oksana2000-02@mail.ru>
2023-11-20 16:26:17 +0300

    all done for page index.html

---
+++ TestSite/page/static/js/script_index.js
@@ -0,0 +1,21 @@
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+arrows: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+});
+
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
+});
